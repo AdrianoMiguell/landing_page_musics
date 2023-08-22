9b1f5ac03826e6d153cdc98848c101d2f9f20aa5
AdrianoMiguell <adrianosantos260804@gmail.com>
2023-08-21 21:52:37 -0300

    seventh-version

--- js/musics.js
+++ js/musics.js
@@ -22,7 +22,7 @@ let get_data = async () => {
 };
 
 let view_musics = async (data) => {
-  for (let d = 0; d < 5; d++) {
+  for (let d = 0; d < 7; d++) {
     let div = document.createElement("div");
     div.setAttribute("class", "four-musics");
     div_musics.appendChild(div);
@@ -30,27 +30,33 @@ let view_musics = async (data) => {
     let div_four_music = document.querySelectorAll(".four-musics");
 
     for (let m = d * 4; m < d * 4 + 4; m++) {
-      let div = document.createElement("a");
-      div.setAttribute("class", "music");
-      div.setAttribute("href", `${data[m].link}`);
-      div.setAttribute("target", "_black");
-      div_four_music[d].appendChild(div);
+      if (data[m].link != undefined) {
+        let div = document.createElement("a");
+        div.setAttribute("class", "music");
+        div.setAttribute("href", `${data[m].link}`);
+        div.setAttribute("target", "_black");
+        div_four_music[d].appendChild(div);
 
-      let div_music = document.querySelectorAll(".music");
+        let div_music = document.querySelectorAll(".music");
 
-      console.log(` d - ${d} ; m - ${m} - realidade: ${m}`);
-      let div_image = document.createElement("div");
-      div_image.setAttribute("class", "image");
-      div_image.innerHTML = `<img src="./img/musics-albums/${data[m].img}" alt="capa da música ${data[m].music}" />`;
-      div_music[m].appendChild(div_image);
+        console.log(` d - ${d} ; m - ${m} - realidade: ${m}`);
+        let div_image = document.createElement("div");
+        div_image.setAttribute("class", "image");
+        div_image.innerHTML = `<img src="./img/musics-albums/${data[m].img}" alt="capa da música ${data[m].music}" />`;
+        div_music[m].appendChild(div_image);
 
-      let div_informs = document.createElement("div");
-      div_informs.setAttribute("class", "informs");
-      div_informs.innerHTML = `
+        let div_informs = document.createElement("div");
+        div_informs.setAttribute("class", "informs");
+        div_informs.innerHTML = `
         <span class="title-music"> ${data[m].music} </span>
         <span class="album-music"> ${data[m].album} - ${data[m].autor} </span>
       `;
-      div_music[m].appendChild(div_informs);
+        div_music[m].appendChild(div_informs);
+
+        if (data[m + 1].link == undefined) {
+          div_four_music[d].classList.add("last_music");
+        }
+      }
     }
   }
 };
@@ -58,7 +64,6 @@ let view_musics = async (data) => {
 let view_albums = async (data) => {
   let order = [];
 
-  const div_albums = document.querySelector(".div_albums");
   const albums = document.querySelector(".albums");
 
   for (let i = 0; i < data.length - 1; i++) {
@@ -67,12 +72,7 @@ let view_albums = async (data) => {
     }
   }
 
-  console.log(order);
-  order.forEach((e) => {
-    console.log(data[e].album);
-  });
-
-  for (let a = 0; a < 10; a++) {
+  for (let a = 0; a < order.length; a++) {
     let album = document.createElement("div");
     album.setAttribute("class", "album");
     albums.appendChild(album);
@@ -88,20 +88,5 @@ let view_albums = async (data) => {
     </div>`;
   }
 };
-
-// {
-/* <div class="div_albums">
-          <h2>Albuns</h2>
-          <div class="albums">
-            <div class="album">
-              <img src="./img/musics-albums/falta_voce-fernando_iglesias.jpg" alt="">
-              <div class="desc_album">
-                <span class="name_album"> Tal </span>
-                <span class="autor"> Album | Adriano </span>
-              </div>
-            </div>
-           
-          </div> */
-// }
 
 start();
